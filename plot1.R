109145cbf4095a508f10f985e2fb5ee7a41690ec
Melissa Clarkson <clarkson_melissa@yahoo.com>
2016-01-09 13:18:02 -0800

    finished homework

---
+++ plot1.R
@@ -0,0 +1,20 @@
+# plot 1 for Course project 1, Exploratory data analysis
+# data from UCI Machine Learning Repository, http://archive.ics.uci.edu/ml/
+# electric power comsuption, https://archive.ics.uci.edu/ml/datasets/Individual+household+electric+power+consumption
+# https://github.com/rdpeng/ExData_Plotting1
+
+# read in data and subset for only first and second days of Feb 2007, format day and time
+data <- read.table("household_power_consumption.txt", sep=";", header = TRUE, na.strings = "?")
+twoDayData <- data[(data$Date == "1/2/2007" | data$Date == "2/2/2007"),  ] 
+daysAndTimes <- paste(twoDayData$Date, twoDayData$Time)
+twoDayData$Time <- strptime(daysAndTimes, format = "%d/%m/%Y %H:%M:%S")
+twoDayData$Date <- strptime(twoDayData$Date, format = "%d/%m/%Y")
+
+# set up png file
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+# create plot, histogram of global active power
+hist(twoDayData$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+
+# stop writing to file
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,24 @@
+# plot 3 for Course project 1, Exploratory data analysis
+# data from UCI Machine Learning Repository, http://archive.ics.uci.edu/ml/
+# electric power comsuption, https://archive.ics.uci.edu/ml/datasets/Individual+household+electric+power+consumption
+# https://github.com/rdpeng/ExData_Plotting1
+
+# read in data and subset for only first and second days of Feb 2007, format day and time
+data <- read.table("household_power_consumption.txt", sep=";", header = TRUE, na.strings = "?")
+twoDayData <- data[(data$Date == "1/2/2007" | data$Date == "2/2/2007"),  ] 
+daysAndTimes <- paste(twoDayData$Date, twoDayData$Time)
+twoDayData$Time <- strptime(daysAndTimes, format = "%d/%m/%Y %H:%M:%S")
+twoDayData$Date <- strptime(twoDayData$Date, format = "%d/%m/%Y")
+
+# set up png file
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
+
+# create plot, time plot of 3 categories of energy sub-metering (largest value in Sub_metering_1, so use that to set up range of plot)
+with(twoDayData, plot(Time, Sub_metering_1, type = "n", ylab = "Energy sub metering", xlab = ""))
+with(twoDayData, lines(Time, Sub_metering_1, col = "black", xlab = "", ylab = ""))
+with(twoDayData, lines(Time, Sub_metering_2, col = "red", xlab = "", ylab = ""))
+with(twoDayData, lines(Time, Sub_metering_3, col = "blue", xlab = "", ylab = ""))
+legend("topright", seg.len = 2, lwd = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# stop writing to file
+dev.off()
